295c5525471b6acf2ff58f1e701991abd2fc51a9
Infectedd <bvslavchev@gmail.com>
2017-01-23 13:13:31 +0100

    major bugfix

--- BronKerbosch.java
+++ BronKerbosch.java
@@ -3,111 +3,90 @@ package graphcol;
 import static graphcol.Main.*;
 
 public class BronKerbosch {
+
+    public static boolean bkPivotRun;
     //some array initialisation for the B-K algorithm
     static int timeIndex = 0;
     public static int maxClique() {
-        timeBK = System.currentTimeMillis();
         int[] np = new int[graph.verticesNumber]; //n for "new"
         for (int i = 0; i < graph.verticesNumber; i++) {
             np[i] = i + 1;
         }
         int[] nr = new int[0];
-        int[] nx = new int[0];
 
-        return bronKerbosch(np, nr, nx).length;
+        int[] pivots = new int[np.length];
+        System.arraycopy(np, 0, pivots, 0, np.length);
+        sort(pivots);
 
+        //System.out.print("pivots = [ ");
+        for (int j = 0; j < pivots.length; j++) {
+            //System.out.print(pivots[j] + " ");
+        }
+        //System.out.print("]\n");
 
+        int cliq = 0;
+
+        for (int i = 0; i < pivots.length && node[pivots[i]].edgeCount > 1; i++) {
+            bkPivotRun = false;
+            //System.out.println("new run with pivot = " + pivots[i]);
+            int ncliq = bronKerbosch(np, nr, pivots[i]).length;
+            if (ncliq > cliq)
+                cliq = ncliq;
+        }
+        return cliq;
     }
 
     // method containing a variation the B-K algorithm for computing the maximum clique, from which the size is equal to the lowerbound of the graph.
-    public static int[] bronKerbosch(int[] p, int[] r, int[] x) {
+    public static int[] bronKerbosch(int[] p, int[] r, int pivot) {
 
         int pivotVtx; // pivot vertex with the highest degree
 
-		/*
-		System.out.print("p = [ ");
-		for (int j = 0; j < p.length; j++) {
-			System.out.print(p[j] + " ");
-		}
-		System.out.print("]\n");
+        //System.out.print("p = [ ");
+        for (int j = 0; j < p.length; j++) {
+            //System.out.print(p[j] + " ");
+        }
+        //System.out.print("]\n");
 
-		System.out.print("r = [ ");
-		for (int j = 0; j < r.length; j++) {
-			System.out.print(r[j] + " ");
-		}
-		System.out.print("]\n");
-		System.out.print("x = [ ");
-		for (int j = 0; j < x.length; j++) {
-			System.out.print(x[j] + " ");
-		}
-		System.out.print("]\n");
-		*/
-
-        //r - possibleClique
-        //p - neighborVertices
-        //x - alreadyUsed
+        //System.out.print("r = [ ");
+        for (int j = 0; j < r.length; j++) {
+            //System.out.print(r[j] + " ");
+        }
+        //System.out.print("]\n");
+        //System.out.println("==================");
 
         // whenever both []p and []x are empty, r contains a maximal clique.
-        if (p.length == 0 && x.length == 0)
+        if (p.length == 0)
             //System.out.println("New maximal clique with size " + r.length + " found!");
-        if (p.length == 0 && x.length == 0 && r.length > graph.maxClique.length) {
+        if (p.length == 0 && r.length > graph.maxClique.length) {
             graph.maxClique = new int[r.length];
-
-            //System.out.println(" " + r.length + " is a new maximum clique! (at least for now)");
-            if(VERBOSE) System.out.println(" Clique time: " + (System.currentTimeMillis() - timeBK) + "ms");
-
             System.arraycopy(r, 0, graph.maxClique, 0, graph.maxClique.length);
-            if(graph.lowerBound < r.length) Main.setLowerBound(r.length);
+            //System.out.println(r.length + " is a new maximum clique! (at least for now) --------------------------------------------------------");
+            Main.setLowerBound(r.length);
             return graph.maxClique;
         }
+
 
         // selecting the pivot vertex.
 
-        /*
-
-        int maxLength = 1; //MAKE THIS ONE
-
-        pivotVtx = -1;
-
-        if(p.length == 0 ) return graph.maxClique;
-        else{
-            for(int i = 0; i < p.length; i++){
-                if(node[p[i]].connectedNodes.length > maxLength){ //MAKE THIS LARGER THAN INSTEAD OF LARGER THAN OR EQUAL
+        if (bkPivotRun) {
+            if (p.length > 0)
+                pivotVtx = p[0];
+            else
+                return graph.maxClique;
+            for (int i = 0; i < p.length; i++) {
+                //System.out.println(node[pivotVtx].connectedNodes.length);
+                if (node[p[i]].connectedNodes.length > node[pivotVtx].connectedNodes.length)
                     pivotVtx = p[i];
-                    maxLength = node[p[i]].connectedNodes.length;
-                }
             }
-        }
-
-        if(pivotVtx == -1) return graph.maxClique;*/
-
-        if (p.length > 0)
-            pivotVtx = p[0];
-        else
-            return graph.maxClique;
-        for (int i = 0; i < p.length; i++) {
-            //System.out.println(node[pivotVtx].connectedNodes.length);
-            if (node[p[i]].connectedNodes.length >= node[pivotVtx].connectedNodes.length)
-                pivotVtx = p[i];
+        } else {
+            pivotVtx = pivot;
+            bkPivotRun = true;
         }
         //System.out.println("Pivot = " + pivotVtx);
-
         int[] pivotArr = {pivotVtx};
         //System.out.println(pivotArr[0]);
 
-        for (int i = 0; i < minus(p, node[pivotVtx].connectedNodes).length; i++) { //some problem with the for-loop!
-            bronKerbosch(inters(p, node[pivotVtx].connectedNodes), union(r, pivotArr), inters(x, node[i+1].connectedNodes));
-            p = minus(p, pivotArr);
-			/*
-			System.out.println("removed " + pivotArr[0] + " from p");
-			System.out.print("p = [ ");
-			for (int j = 0; j < p.length; j++) {
-				System.out.print(p[j] + " ");
-			}
-			System.out.print("] was changed\n");
-			*/
-            x = union(x, pivotArr);
-        }
+        bronKerbosch(inters(p, node[pivotVtx].connectedNodes), union(r, pivotArr), pivot);
 
         return graph.maxClique;
     }
@@ -236,5 +215,35 @@ public class BronKerbosch {
         System.arraycopy(a1, 0, a, 0, nLength);
 
         return a;
+    }
+
+    public static void sort(int a[]) {
+        sort(a, 0, a.length);
+    }
+
+    private static void sort(int a[], int l, int n) {
+        if(n <= 1)
+            return;
+        swap(a, l+n-1, l+(int)(Math.random()*n));
+        int i = l, k = l, p = l+n;
+        while(i < p) {
+            if(node[a[i]].edgeCount > node[a[l+n-1]].edgeCount)
+                swap(a, i++, k++);
+            else if(node[a[i]].edgeCount == node[a[l+n-1]].edgeCount)
+                swap(a, i, --p);
+            else
+                i++;
+        }
+        int m = Math.min(p-k, l+n-p+1);
+        for(int j = 0; j < m-1; j++)
+            swap(a, k+j, l+n-m+1+j);
+        sort(a, l, k-l);
+        sort(a, k+1, l+n-k-1);
+    }
+
+    public static void swap(int a[], int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 }
